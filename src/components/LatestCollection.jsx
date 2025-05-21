f25236ab9bcb377a7b4d3f723d1bdd5ebd35581e
Rayhana786 <r.khelifati@esi-sba.dz>
2025-05-21 15:05:56 +0100

    djhfu

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import { useContext, useEffect, useState } from "react";
-import { ShopContext } from "../context/ShopContext";
-import Title from "./Title";
-import ProductItem from "./ProductItem";
-
-const LatestCollection = () => {
-  const { products } = useContext(ShopContext);
-  // console.log(products);
-  const [latestProduct, setLatestProduct] = useState([]);
-  useEffect(() => {
-    setLatestProduct(products.slice(2, 12));
-  }, []);
-  return (
-    <div className="flex flex-col gap-10 ">
-      <Title text1={"LATEST"} text2={"COLLECTIONS"} />
-      <div className="grid lg:grid-cols-5 md:grid-col-3 sm:grid-cols-2 grid-cols-1 gap-6 justify-items-center   ">
-        
-          {latestProduct.map((p,index) => (
-            <ProductItem
-              name={p.name}
-              id={p.id}
-              image={p.image}
-              price={p.price}
-              key={index}
-            />
-          ))}
-        
-      </div>
-      {/*
-       */}
-    </div>
-  );
-};
-=======
 import { useEffect, useState } from "react";
 import Title from "./Title";
 import ProductItem from "./ProductItem";
@@ -65,5 +30,4 @@ const LatestCollection = () => {
   );
 };
 
->>>>>>> 83b28a122e6991040aa412dd99885b36616d6165
 export default LatestCollection;
